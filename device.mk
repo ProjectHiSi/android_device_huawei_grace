d094c179219aa0c3e7a6f4a90183728f0a510098
Huawei-Dev <surdu_petru@yahoo.com>
2018-11-10 09:24:59 +0100

    make device better explicitly

--- device.mk
+++ device.mk
@@ -16,14 +16,31 @@
 
 $(call inherit-product-if-exists, vendor/huawei/grace/grace-vendor.mk)
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
 # Audio
+PRODUCT_PACKAGES += \
+    audio.primary.default \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    libaudioutils \
+    libtinyalsa \
+    tinyplay \
+    tinycap \
+    tinymix \
+    tinypcminfo
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/prebuilts/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/prebuilts/topazhp.cfg:system/etc/topazhp.cfg
+    $(LOCAL_PATH)/prebuilts/audio_policy.conf:system/etc/audio_policy.conf
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    bt_vendor.conf
+
+# Default properties
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.enable_boot_charger_mode=0 \
+    persist.sys.usb.config=manufacture,adb
 
 # GPS
 PRODUCT_COPY_FILES += \
@@ -31,6 +48,23 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/prebuilts/gps47531config.xml:system/etc/gps47531config.xml \
     $(LOCAL_PATH)/prebuilts/gps47531config_beta.xml:system/etc/gps47531config_beta.xml
+
+# HWC Hal
+PRODUCT_PACKAGES += \
+    hwcomposer.hi3635
+
+# KEYPAD
+PRODUCT_PACKAGES += \
+    usbaudio.kl
+
+# libGLES_android
+PRODUCT_PACKAGES += \
+    libGLES_android
+
+# LibShim
+PRODUCT_PACKAGES += \
+    libshim_icu \
+    libshim_gui
 
 # Media configuration
 PRODUCT_COPY_FILES += \
@@ -43,10 +77,30 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
-# Thermal
+# NFC
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    Tag \
+    NfcNci \
+    nfc_nci.pn54x.default
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/thermald.xml:system/etc/thermald.xml \
-    $(LOCAL_PATH)/prebuilts/thermald_performance.xml:system/etc/thermald_performance.xml
+    $(LOCAL_PATH)/prebuilts/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/prebuilts/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/prebuilts/nfcee_access.xml:system/etc/nfcee_access.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
+
+# OMX Codec
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/topazhp.cfg:system/etc/topazhp.cfg
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -78,65 +132,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.app_widgets.xml:system/etc/permissions/android.software.app_widgets.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
-# Screen density
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
-PRODUCT_TAGS += dalvik.gc.type-precise
-
-# Audio
-PRODUCT_PACKAGES += \
-    audio.primary.default \
-    audio.a2dp.default \
-    audio.usb.default \
-    audio.r_submix.default \
-    libaudioutils \
-    libtinyalsa \
-    tinyplay \
-    tinycap \
-    tinymix \
-    tinypcminfo
-
-# BT
-PRODUCT_PACKAGES += \
-    bt_vendor.conf
-
-# hwcomposer
-PRODUCT_PACKAGES += \
-    hwcomposer.hi3635
-
-# KEYPAD
-PRODUCT_PACKAGES += \
-    usbaudio.kl
-
-# libGLES_android
-PRODUCT_PACKAGES += \
-    libGLES_android
-
-# LibShim
-PRODUCT_PACKAGES += \
-    libshim_icu \
-    libshim_gui
-
-# NFC
-PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    Tag \
-    NfcNci \
-    nfc_nci.pn54x.default
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/prebuilts/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/prebuilts/nfcee_access.xml:system/etc/nfcee_access.xml
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
-
-# Power
+# Power Hal
 PRODUCT_PACKAGES += \
     power.hi3635
 
@@ -151,13 +147,20 @@ PRODUCT_PACKAGES += \
     init.hi3635.usb.rc \
     ueventd.hi3635.rc
 
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/thermald.xml:system/etc/thermald.xml \
+    $(LOCAL_PATH)/prebuilts/thermald_performance.xml:system/etc/thermald_performance.xml
+
 # USB
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.enable_boot_charger_mode=0 \
-    persist.sys.usb.config=manufacture,adb
 
 # Wifi
 PRODUCT_PACKAGES += \
